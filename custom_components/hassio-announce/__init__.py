80ed6c6342e578b5089611ac18847ac65f2bd670
louisfellows <louisfellows@gmail.com>
2024-12-03 17:14:30 +0000

    removed config from init

--- custom_components/hassio-announce/__init__.py
+++ custom_components/hassio-announce/__init__.py
@@ -1,18 +1,1 @@
-"""The MusicNode integration."""
-from __future__ import annotations
-
-import logging
-
-from homeassistant.core import HomeAssistant
-from homeassistant.helpers.typing import ConfigType
-
-from .const import DOMAIN
-
-LOGGER = logging.getLogger(__name__)
-
-# For your initial PR, limit it to 1 platform.
-PLATFORMS: list[Platform] = [Platform.MEDIA_PLAYER]
-
-def setup(hass: HomeAssistant, config: ConfigType) -> bool:
-    """Set up a skeleton component."""
-    return True
+"""The hassio-announce integration."""
